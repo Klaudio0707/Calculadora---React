11cf62d22e471269f263a3200ca07452ebe1f0d9
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-08-03 17:10:21 -0300

    atualizaçao

---
+++ src/components/Input/styles.js
@@ -0,0 +1,36 @@
+
+
+import styled from 'styled-components';
+
+
+export const InputContainer = styled.div`
+width: 100%;
+height: 75px;
+background-color: #aaaaff;
+
+display: flex;
+align-items; center;
+justify-content: flex-end;
+
+font-size; 24px;
+font-family: 'Roboto';
+
+
+input {
+width: 100%;
+height: 75px;
+background-color: #aaaaff;
+border: 0;
+display: flex;
+flex-direction: colum;
+align-items: felx-end;
+
+
+font-size: 24px;
+font-family: 'Roboto';
+padding: 0 10px;
+color: #ffffff;
+}
+
+
+`
